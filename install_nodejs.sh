b70e5e2a9ab3018123f4132c758256b6d12df57d
liangduanqi <1125191117@qq.com>
2021-03-03 19:30:15 +0800

    fix

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 # 安装nodejs
+apt-get update & apt-get install nodejs
 
-wget https://nodejs.org/dist/v8.11.4/node-v8.11.4.tar.gz
-tar -xzvf node-v8.11.4.tar.gz
-cd node-v8.11.4
-./configure --prefix=/usr/local/node; make install
-export EXECJS_RUNTIME=/usr/local/node
 echo "安装nodejs 完成"
