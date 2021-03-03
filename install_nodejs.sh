a7dec0fc9a111d61e108bc0ebc0ce0d1857735ec
liangduanqi <1125191117@qq.com>
2021-03-03 16:35:27 +0800

    fix

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -2,5 +2,7 @@
 # 安装nodejs
 
 wget https://nodejs.org/dist/v8.11.4/node-v8.11.4.tar.gz
+tar -xzvf node-v8.11.4.tar.gz
+cd node-v8.11.4
 ./configure --prefix=/usr/local/node; make install
 export EXECJS_RUNTIME=/usr/local/node
